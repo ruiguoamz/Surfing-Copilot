b4ef58a7465fab2d30061b81b4e9117bf131d5b8
RuiGuo <48600426+ruiguoamz@users.noreply.github.com>
2023-05-10 12:28:09 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,9 +1,3 @@
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        console.log(request.html);
-    }
-);
-
 chrome.tabs.onUpdated.addListener(
     function(request, sender, sendResponse) {
         console.log(request.html);
